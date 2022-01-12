91b574e4d8251a5d96b0594b1e9f314748d166da
johmert <johmert@users.noreply.github.com>
2022-01-11 17:05:23 -0800

    new components made and separated

--- src/pomodoro/Duration.js
+++ src/pomodoro/Duration.js
@@ -1,5 +1,32 @@
 import React from "react";
 
-function Duration() {}
+function Duration({label, time}) {
+    return (
+        <div className="input-group input-group-lg mb-2">
+        <span className="input-group-text" data-testid="duration-focus">
+          {/* TODO: Update this text to display the current focus session duration */}
+            {label} Duration: {time}
+        </span>
+        <div className="input-group-append">
+          {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
+          <button
+            type="button"
+            className="btn btn-secondary"
+            data-testid="decrease-focus"
+          >
+            <span className="oi oi-minus" />
+          </button>
+          {/* TODO: Implement increasing focus duration and disable during a focus or break session */}
+          <button
+            type="button"
+            className="btn btn-secondary"
+            data-testid="increase-focus"
+          >
+            <span className="oi oi-plus" />
+          </button>
+        </div>
+      </div>
+    );
+}
 
 export default Duration;
