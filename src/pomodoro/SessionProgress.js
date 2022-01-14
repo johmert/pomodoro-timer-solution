3fe815a6725b440d697e3c765912019af1783359
johmert <johmert@users.noreply.github.com>
2022-01-13 23:34:44 -0800

    progress bar visually ticks down

--- src/pomodoro/SessionProgress.js
+++ src/pomodoro/SessionProgress.js
@@ -5,6 +5,8 @@ function SessionProgress({session, time, isTimerRunning}) {
   if(!isTimerRunning) return null;
   const countdown = secondsToDuration(session?.timeRemaining);
   const showTimeRemaining = countdown + " remaining";
+  const percent = (session?.timeRemaining / (time * 60)) * 100;
+  console.log(percent);
 
   return (
     <div>
@@ -26,8 +28,8 @@ function SessionProgress({session, time, isTimerRunning}) {
             role="progressbar"
             aria-valuemin="0"
             aria-valuemax="100"
-            aria-valuenow={countdown / time} // TODO: Increase aria-valuenow as elapsed time increases
-            style={{ width: `${countdown/time}` }} // TODO: Increase width % as elapsed time increases
+            aria-valuenow={percent} // TODO: Increase aria-valuenow as elapsed time increases
+            style={{ width: `${percent}%` }} // TODO: Increase width % as elapsed time increases
           />
         </div>
       </div>
